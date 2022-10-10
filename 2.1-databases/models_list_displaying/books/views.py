aa1f99f1602392c2ea165bcb75740c8a89b3ec35
Dimasuz <dimaskus@gmail.com>
2022-10-10 19:55:21 +0300

    HW_5.3.2

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,33 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+import datetime
+from books.models import Book
+
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books_objects = Book.objects.all()
+    context = {'books' : books_objects, }
     return render(request, template, context)
+
+
+def books_review(request, year, month, day):
+    template = 'books/books_review.html'
+    page_number = request.GET.get('page')
+    date = datetime.date(year, month, day)
+    books_obj = Book.objects.all().order_by('pub_date')
+    paginator = Paginator(books_obj, 1)
+    if page_number is None:
+        page_numbers = paginator.num_pages
+        for x in range(1, page_numbers + 1):
+            page = paginator.get_page(x)
+            for p in page:
+                if date == p.pub_date:
+                    page_number = x
+                    break
+
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj, }
+    return render(request, template, context)
+
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from books.views import books_view
+import books.views
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('', books.views.books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('books/', books.views.books_view, name='books'),
+    path('books/<int:year>-<int:month>-<int:day>/', books.views.books_review, name='book'),
 ]
